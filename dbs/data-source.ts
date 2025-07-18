b82307ae0dd1e205e9f507499559bcc5fddd34f5
Rasheed Ayoade <rashotech@gmail.com>
2025-07-18 11:02:25 +0100

    remove data source configuration file

--- dbs/data-source.ts
+++
@@ -1,5 +0,0 @@
-import { dataSourceOptions } from "src/config/data-source-options";
-import { DataSource } from "typeorm";
-
-const dataSource = new DataSource(dataSourceOptions);
-export default dataSource;
