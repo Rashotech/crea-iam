a5660a95cab0fa8f076f5d6b791d21461852665c
Rasheed Ayoade <rashotech@gmail.com>
2025-07-18 08:18:52 +0100

    setup typeorm and postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import envConfiguration from './config/env';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -10,6 +11,20 @@ import { ConfigModule } from '@nestjs/config';
       load: [envConfiguration],
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: ["dist/**/*.entity.js"],
+        migrations: ["dist/db/migrations/*.js"],
+      }),
+      inject: [ConfigService],
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,11 @@ dotenv.config();
 export default () => ({
   port: parseInt(process.env.PORT ?? '3000', 10) || 3000,
   environment: process.env.NODE_ENV ?? 'development',
+  postgres: {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    db: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  },
 });
