76c74f021eeead6c0da0f9e7483bab97394507d1
Rasheed Ayoade <rashotech@gmail.com>
2025-07-18 10:11:03 +0100

    remove debug log for data source initialization

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,5 @@
 import { dataSourceOptions } from "src/config/data-source-options";
 import { DataSource } from "typeorm";
 
-console.log("Initializing data source with options:", dataSourceOptions);
 const dataSource = new DataSource(dataSourceOptions);
 export default dataSource;
